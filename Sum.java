75a5960b83cab20e5c14044f3cda3e0ccc730826
Jeevaluxshy06 <121012140+Jeevaluxshy06@users.noreply.github.com>
2023-01-02 14:21:42 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ class Sum{
     public static void main(String args[]){
         int a = 3;
         int b = 8;
-        System.out.println(a+); //summation
+        System.out.println(a+b); //summation
     }
 }
